b510c83e27497f1fc041c4dc9db204efed037a5a
AM <72007554+sinskey@users.noreply.github.com>
2025-10-08 08:28:17 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "Nautica - Sinskey";
+let APP_DOMAIN = "sinskey.alkaida.dpdns.org";
 
 let prxIP = "";
 let cachedPrxList = [];
